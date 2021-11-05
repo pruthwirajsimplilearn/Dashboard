675624f45481203c463e452bf4af8a0e8649d920
mjmaurya <gcssm1999@gmail.com>
2021-11-05 13:29:58 +0530

    Navbar Added

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Home from "./components/Home";
 import Login from "./components/Login";
+import { NavBar } from "./components/NavBar";
 import Register from "./components/Register";
 
 function App() {
   return (
     <Router>
+      <NavBar/>
       <Route path="/" exact>
         <Home/>
       </Route>
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,28 @@
+export const NavBar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="/">Project Report</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="/">Home </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/features">Features</a>
+                    </li>
+                </ul>
+                <div className="navbar-nav ml-auto d-flex">
+                    <li className="nav-item">
+                        <a className="nav-link" href="/login">Login</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link " href="/register">Sign Up</a>
+                    </li>
+                    </div>
+            </div>
+        </nav>
+    )
+}
